9e6d179bf41c911777b0cd29de5f6c7c28282dc6
Neelesh <53299497+neeleshcrasto@users.noreply.github.com>
2019-12-19 23:28:24 +0530

    Add files via upload

---
+++ MIT/assn1.py
@@ -0,0 +1,24 @@
+n = int(input('Enter which n(th) Prime number you want: '))
+
+# for first prime number
+if n == 1:
+    prime = 2
+    print ('The no.',n,'prime number is', prime)
+
+
+    
+
+# Generate number
+if n > 1:
+    nos = 3
+    i = 2
+    while i <= n:
+        if nos%i = 0:
+            i = i + 1
+
+
+            
+
+
+
+# check if number is prime & print it out
