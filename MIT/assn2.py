f53197473d687c7177ae3c068f263789a249655a
Neelesh <53299497+neeleshcrasto@users.noreply.github.com>
2019-12-27 17:22:54 +0530

    Update assn2.py

--- MIT/assn2.py
+++ MIT/assn2.py
@@ -2,31 +2,56 @@
 ## 6a + 9b + 20c = n ##
 #---------------------------------#
 
+import numpy
+
 #---------------------------------#
 ## Solution to Problem 1 ##
 #---------------------------------#
 
 n = int(input('Enter number of McNuggets you want:'))
 
-intc = n // 20
-intb = n // 9
-inta = n // 6
+lists = [6, 9, 15, 20, 26, 29, 35]
 
-intab = n // 15
-intac = n // 26
-intbc = n // 29
+def secondlvl(rem):
+    rem2 = rem%list
+    if rem2 in lists or rem2 == 0:
+        print(n//list,'buxes of',list,'McNuggets')
+        print('1 bux of',rem2,'McNuggets')
+        quit()
 
-intabc = n // 35
 
-rema = n%6
-remb = n%9
-remc = n%20
 
-remab = n%15
-remac = n%26
-rembc = n%29
+for list in lists:
+    rem = n%list
+    if rem in lists or rem == 0:
+        print(n//list,'boxes of',list,'McNuggets')
+        print('1 box of',rem,'McNuggets')
+        quit()
+            
+secondlvl(rem)
 
-remabc = n%35
 
-print('a=',inta,',b=',intb,',c=',intc,',ab=',intab,',ac=',intac,',bc=',intbc,',abc=',intabc)
-print('rem a=',rema,',rem b=',remb,', rem c=',remc,', rem ab=',remab,', rem ac=',remac,', rem bc=',rembc,', rem abc=',remabc)
+print(n,'McNuggets cannot be purchased')
+
+# intc = n // 20
+# intb = n // 9
+# inta = n // 6
+
+# intab = n // 15
+# intac = n // 26
+# intbc = n // 29
+
+# intabc = n // 35
+
+# rema = n%6
+# remb = n%9
+# remc = n%20
+
+# remab = n%15
+# remac = n%26
+# rembc = n%29
+
+# remabc = n%35
+
+# print('a=',inta,',b=',intb,',c=',intc,',ab=',intab,',ac=',intac,',bc=',intbc,',abc=',intabc)
+# print('rem a=',rema,',rem b=',remb,', rem c=',remc,', rem ab=',remab,', rem ac=',remac,', rem bc=',rembc,', rem abc=',remabc)
