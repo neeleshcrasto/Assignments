ff0127fb636f752a7640290ce90f999b60063d0b
Neelesh <53299497+neeleshcrasto@users.noreply.github.com>
2020-04-14 20:17:11 +0530

    Add files via upload

--- Ciphers/vigenere.py
+++ Ciphers/vigenere.py
@@ -1,24 +1,54 @@
 # Vignere cipher
 
-# This section encrypts the text by shifting it right
+# This section encrypts the text by using the key
 def encrypt():
     plain_text = input('Enter the text to encrypt: ')
     plain_text = plain_text.upper()
-    shift = int(input('Enter the shift number: '))
+    length = len(plain_text)
+
+    key = str(input('Enter the key: '))
+    full_key = key.upper()*(1+(length//(len(key))))
     encrypt_char = []
-    for char in plain_text:
-        plain_no = ord(char)
+    for index_no in range(0,length):
+        plain_no = ord(plain_text[index_no])
+        shift = (ord(full_key[index_no]) - 65)
         if 64 < plain_no < 91:
-            encrypt_no = plain_no - shift
-            if encrypt_no < 65:
-                encrypt_no = (91 - (65 - encrypt_no))
-        elif 47 < plain_no < 58:
-            encrypt_no = plain_no - shift
-            if encrypt_no < 48:
-                encrypt_no = (58 - (48 - encrypt_no))
+            encrypt_no = plain_no + shift
+            if encrypt_no > 90:
+                encrypt_no = (64 + (encrypt_no - 90))
         else:
             encrypt_no = plain_no
         encrypt_char.append(chr(encrypt_no))
 
     print('Plain text:',plain_text)
-    encrypt_text = print(''.join(encrypt_char))
+    encrypt_text = print('Encrypted text:',''.join(encrypt_char))
+
+# This section decrypts the text by using the key
+def decrypt():
+    encrypt_text = input('Enter the text to decrypt: ')
+    encrypt_text = encrypt_text.upper()
+    length = len(encrypt_text)
+
+    key = str(input('Enter the key: '))
+    full_key = key.upper()*(1+(length//(len(key))))
+    decrypt_char = []
+    for index_no in range(0,length):
+        encrypt_no = ord(encrypt_text[index_no])
+        shift = (ord(full_key[index_no]) - 65)
+        if 64 < encrypt_no < 91:
+            plain_no = encrypt_no - shift
+            if plain_no < 65:
+                plain_no = (91 - (65 - plain_no))
+        else:
+            plain_no = encrypt_no
+        decrypt_char.append(chr(plain_no))
+
+    print('Encrypted text:',encrypt_text)
+    decrypt_text = print('Decrypted text:',''.join(decrypt_char))
+
+
+selection = input('What do you want to do? Encrypt(E) or Decrypt(D): ')
+if selection == 'e':
+    encrypt()
+elif selection == 'd':
+    decrypt()
